2b9960501f00a49de6ffc8008fef5a445aa41e1b
zero_asus <zero@ASUS.>
2024-08-02 18:11:35 -0500

    working on

---
+++ anki/words.py
@@ -0,0 +1,50 @@
+'''
+    Get Words Databases
+    clean_text = re.sub(r"\n\d+ \| \d+$", "FREIRE", text)
+    \* [a-zA-Z\u00C0-\u017F, '-.?!\d:;%»«çœ]+\n
+    \* [a-zA-Z\u00C0-\u017F ,"'-.?!\d:;%»«çœ$/]+\n
+    (\d+ \| \d+)
+    ^[^*\s\d\w\u00C0-\u017F].*
+'''
+from ast import pattern
+from PyPDF2 import PdfReader
+import re
+
+
+pdf = PdfReader('french_frequancy_words.pdf')
+number_of_pages = len(pdf.pages)
+page = pdf.pages[17]
+print(page.extract_text())
+#pattern_sub = r"(\n\d+ \| \d+[\+\- lns]+\n\d{1})|(\n\d+ \| \d+\+n\n\d{1})|(\n\d+ \| \d+\n\d{1})|(\n\d+ \| \d+\+s\n\d{1})|(\d+ \| \d+\n\d{1})"
+
+with open('pdf.txt', 'w', encoding='utf-8') as file:
+    for i in range(17,477):
+        print(f"Writing page {i}")
+        page = pdf.pages[i]
+        text = page.extract_text()
+        #text = re.sub(pattern_sub, " \n", text, flags=re.MULTILINE)
+        file.write(text)
+        
+
+
+with open('clean_pdf.txt', 'r', encoding='utf-8') as file:
+    content = file.read()
+
+# Busca coincidencias utilizando la expresión regular
+matches = re.findall(r"(^\d+ [\w ,;\(\)]+$)|(^\* [\w, '-]+)", content, re.MULTILINE)
+matches_2 = re.findall(r"^\d+ [\w ,;\(\)]+\n^\* [\w, '-]+", content, re.MULTILINE)
+# Imprime las coincidencias
+print(len(matches))
+
+
+print("\n\n"+"-"*20+"\n\n")
+
+print(len(matches_2))
+with open("matches.txt", "w") as texto:
+    for index, match in enumerate(matches_2):
+        print(f"index: {index}")
+        print(match)
+        print(repr(str(match).split(" ")[0]))
+        print("\n")
+        texto.write(match)
+
